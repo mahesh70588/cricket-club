7b1fd34b4a6eb71cc83c8f6e4dc8daa8344befcd
mahesh70588 <138879761+mahesh70588@users.noreply.github.com>
2024-02-05 21:54:20 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,19 +1,23 @@
-<nav class="navbar navbar-expand-lg " >
+<nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <header>
-    <a class="navbar-brand" href="index.php" >R'45 Cricket Academy</a>
-</header>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="d-flex">
-          <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
-          <a class="nav-link active" aria-current="page" href="About.html">About</a>
-          <a class="nav-link active" aria-current="page" href="Gallery.html">Gallery</a>
-        </li>
-      </ul>
+        <header>
+            <a class="navbar-brand" href="index.php">R'45 Cricket Academy</a>
+        </header>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="About.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="Gallery.html">Gallery</a>
+            </li>
+        </ul>
         <form class="d-flex">
-        <a class="btn btn-outline mx-2" type="submit" href="signup.php">Sign Up </a>
-        <a class="btn btn-outline mx-2" type="submit" href="login.php">Log in</a>
-        <a class="btn btn-outline mx-2" type="submit" href="logout.php">Logout</a>
+            <a class="btn btn-outline mx-2" href="signup.php">Sign Up</a>
+            <a class="btn btn-outline mx-2" href="login.php">Log in</a>
+            <a class="btn btn-outline mx-2" href="logout.php">Logout</a>
         </form>
     </div>
 </nav>
